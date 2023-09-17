28c51388525baae0dd70f795b4812ec0b6b076aa
kareem_sulaimon <106354820+KareemSulaimon@users.noreply.github.com>
2023-09-17 23:45:43 +0100

    hello list

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -4,7 +4,7 @@ import {AiOutlineDown} from 'react-icons/ai';
 import { useParams } from 'react-router-dom';
 import { useStateContext } from '../context/StateContext';
 import play from '../assets/play.svg';
-import list from '../assets/list.svg';
+import List from '../assets/List.svg';
 import ticket from '../assets/ticket.svg';
 import movie from '../assets/movie.svg';
 import show from '../assets/TV Show.svg';
@@ -99,7 +99,7 @@ function Movie() {
                 <h3>See Showtimes </h3> 
             </button>
               <button className='flex items-center w-full gap-1 justify-center px-3 py-3 bg-white border border-solid border-red-200 font-bold  rounded text-gray-600  text-xl'>
-              <img src={list} alt="list icon" />     
+              <img src={List} alt="list icon" />     
                 <h3>More watch options </h3> 
             </button>
             <div className='flex relative items-center w-full '>
